d377f1d64691a039b9167373f08323c847de04d2
buihien0109 <buihien01091997@gmail.com>
2022-05-14 16:08:39 +0700

    update

--- Lesson09/3Abstract/src/main/java/demo_abstract/Test.java
+++ Lesson09/3Abstract/src/main/java/demo_abstract/Test.java
@@ -1,10 +1,13 @@
 package demo_abstract;
 
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
         // Khởi tạo đối tượng từ abstract class --> Báo lỗi
         // Employee employee = new Employee();
 
+        // ****************************
         // Khởi tạo đối tượng Developer
         Developer developer = new Developer(1, "Bùi Hiên", "hien@gmail.com", 1_000_000, 20);
 
@@ -19,5 +22,18 @@ public class Test {
         // Gọi các phương thức của developer
         tester.register();
         System.out.println(tester.calculateSalary());
+
+        // ****************************
+        // Tính lương cho 1 danh sách các đối tượng
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Developer(1, "Bùi Hiên", "hien@gmail.com", 1_000_000, 20));
+        employees.add(new Developer(2, "Minh Duy", "duy@gmail.com", 2_000_000, 30));
+        employees.add(new Tester(3, "Thu Hằng", "hang@gmail.com", 2_000_000, 30));
+        employees.add(new Tester(4, "Bích Ngọc", "ngoc@gmail.com", 3_000_000, 20));
+
+        // Tính lương
+        for (Employee e: employees) {
+            System.out.println(e.calculateSalary());
+        }
     }
 }
